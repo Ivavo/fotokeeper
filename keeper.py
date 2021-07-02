c4ddd3a8393a1d659c7d943b42222cdbc9d09726
root <lernpyiv@gmail.com>
2021-07-02 17:05:22 +0300

    commit

--- keeper.py
+++ keeper.py
@@ -38,7 +38,7 @@ def read_log(file_name):
         return images_log
 
 
-def uploader(log_name, dir_name):
+def uploader(log_name, dir_name='/run/user/1000/gvfs/smb-share:server=freenas.local,share=disk/a'):
     dir_name = dir_name
     files_list = read_log(log_name)
     p = 0
@@ -46,18 +46,20 @@ def uploader(log_name, dir_name):
         p += 1
         route = file
         print(file)
-        try:
-            source = f'/home/pc/project5/photokeeper/{route}'
-            if os.path.exists(source):
-                result_path = dir_name
-                move = shutil.move
-                move(source, result_path)
-                print(p)
-            else:
-                print('not')
-        except OSError:
+        if route != '':
+            try:
+                source = f'/home/pc/project5/photokeeper/{route}'
+                if os.path.exists(source):
+                    result_path = dir_name
+                    move = shutil.move
+                    move(source, result_path)
+                    print(p)
+                else:
+                    print('not')
+            except OSError:
+                pass
+        else:
             pass
-
 
 _route = pathlib.Path(__file__).parents[1]
 log_name = 'log.txt'
@@ -73,6 +75,7 @@ def image_seeker(images_route):
     for file in status:
         img_name = file
         files_path = os.path.join(_route, img_name)
+        print(files_path)
         try:
             file_format = magic.from_file(files_path, mime=True).split('/')
         except IsADirectoryError:
@@ -112,7 +115,8 @@ def main():
                         images_log = read_log(log_name)
                         log_number = len(images_log)
                         current_number = len(images)
-                        if images_log != images:
+                        print(images, current_number, log_number)
+                        if current_number > log_number or current_number == 1:
                             difference = list(set(images) - set(images_log))
                             file_name = 'difference_log.txt'
                             archive_name = 'New.zip'
@@ -121,15 +125,17 @@ def main():
                                 zipping(difference, 'w', archive_name)
                                 print('difference_list not found')
                             else:
-
                                 logging(difference, file_name)
-                                print('ttt', difference, len(difference), sep='\n')
+                                print('ttt')
                                 #zipping(difference, 'a', archive_name)
-                        else:
-                            dif = read_log('difference_log.txt')
-                            logging('', 'difference_log.txt')
-                            print(len(dif))
+                                uploader('difference_log.txt')
+                        elif current_number <= log_number:
                             pass
+                        #else:
+                        #    dif = read_log('difference_log.txt')
+                        #    logging('0', 'difference_log.txt')
+                        #    print(len(dif))
+                        #    pass
 
                     except FileNotFoundError:
                         pass
